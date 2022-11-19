ed030ec369e91fd29ef3a576254927f4a07e239e
khachgevorgyan94 <khachgevorgyan94@gmail.com>
2022-11-20 01:17:56 +0400

    new lesson

--- src/App.js
+++ src/App.js
@@ -1,28 +1,122 @@
 import React from "react";
-import Home from "./pages/home";
-import Header from "./components/header";
-import Footer from "./components/footer";
-
-// compomnentnern ogtagorcelu hamar  pete anpayman inport anel,  isk  import anelu hamar
-// ayd  fayl@ stexcelcu heto  tvyal  funkcian  petq e  export anel
 
 
 class App extends React.Component {
-  const
-  getMyName = () => {
 
-    return {}
+  state = {
+    formData: {
+      firstName: '',
+      lastName: '',
+      email: '',
+      address: '',
+      password: ''
+    },
+    errorData: {
+      firstName: '',
+      lastName: '',
+      email: '',
+      address: '',
+      password: ''
+    }
+  }
+
+  onChangeFirstName = (e) => {
+    //    lav tarberak
+    this.setState({
+      formData: {
+        ...this.state.formData,
+        [e.target.name]: e.target.value
+      }
+    })
+
+    //  vat  tarberak
+    // if(e.target.name==='lastName'){
+    //   this.setState({formData:  {...this.state.formData,lastName:e.target.value}})
+    // }
+    // if(e.target.name==='email'){
+    //   this.setState({formData:{...this.state.formData,email:e.target.value}})
+    // }
+    // if(e.target.name==='address'){
+    //   this.setState({formData:{...this.state.formData,address:e.target.value}})
+    // }
+  }
+
+  handleClick = () => {
+    const {formData, errorData} = this.state
+    const newErrorData = {
+      firstName: '',
+      lastName: '',
+      email: '',
+      address: '',
+      password: ''
+    }
+
+
+    if (!formData.firstName.trim().length) {
+      newErrorData.firstName = 'Lracreq Anun@'
+    }
+    if (!formData.lastName.trim().length) {
+      newErrorData.lastName = 'Lracreq Azganun@'
+    }
+
+    this.setState({errorData: newErrorData})
+    console.log(formData)
   }
 
   render() {
-    // return aneluc  chenq  karox  miajamanak  mi qani ban return anel
-    // ayd  patcharov  ogtagorcum eqn <></> datark  tegern  mi qani  ban  xmbavorvac durs hanelu hamar
-    // componetnenr  steceluc  heto  erb export en anum  aystex  grum eqn  import arac  componenti anun  tegi  tesqov
-    // mer  bolor  faylern sarqum eqn voch te .js  ayc  canckac fayl stexceluc  sarqum eqn  .jsx  tesaki  fayler
+    const {formData, errorData} = this.state
     return <>
-      <Header></Header>
-      <Home></Home>
-      <Footer></Footer>
+      <div>
+        <div>
+          <label>
+            <input name={'firstName'}
+                   onChange={this.onChangeFirstName}
+                   value={formData.firstName}
+                   placeholder='first name'
+                   className='P-inputs' type="text"/>
+
+            {errorData.firstName ? <p>{errorData.firstName}</p> : null}
+          </label>
+        </div>
+        <div>
+          <label>
+            <input name={'lastName'}
+                   onChange={this.onChangeFirstName}
+                   value={formData.lastName}
+                   placeholder='last name' className='P-inputs' type="text"/>
+          </label>
+        </div>
+        <div>
+          <label>
+            <input name={'email'}
+                   onChange={this.onChangeFirstName}
+                   value={formData.email}
+                   placeholder='email' className='P-inputs' type="text"/>
+          </label>
+        </div>
+        <div>
+          <label>
+            <input name={'address'}
+                   onChange={this.onChangeFirstName}
+                   value={formData.address}
+                   placeholder='address'
+                   className='P-inputs'
+                   type="text"/>
+          </label>
+        </div>
+        <div>
+          <label>
+            <input name={'password'}
+                   onChange={this.onChangeFirstName}
+                   value={formData.password}
+                   placeholder='password'
+                   className='P-inputs' type="password"/>
+          </label>
+        </div>
+      </div>
+      <button onClick={this.handleClick}>
+        clock me
+      </button>
     </>
   }
 }
