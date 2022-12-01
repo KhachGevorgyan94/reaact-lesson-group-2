9c36340bbf45f38bc4025f9a27434624b0c34b6b
khachgevorgyan94 <khachgevorgyan94@gmail.com>
2022-12-02 00:22:58 +0400

    input changes

--- src/App.js
+++ src/App.js
@@ -1,90 +1,54 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import './App.css'
-import CardBox from "./components/card-box";
-import TabList from "./components/tab-list";
-
-//  functional component 1
-// const App = ()=>{
-//
-//   return <div>
-//     asdfasdfasdf
-//   </div>
-// }
-
-// export default App
-
-
-
-/**
- * Functional  componetn @ react jsi  jamanakakic  methodneric mekn e  vor@  tuyl e talis  stexcel  componentner aranc  classi mijnordutyamb,
- * ayn karox e irenic nerkayacnel hasarak  function kam arrow  function
- * vor@  veradarcnu me  html  tesaki  model  {return e anum html tesaki model}
- * bolor  hnaravorutyunner@ voronq kirarum eqn  class  componentneri jamanak  hasaneli en  nayev  functional compomnentneri hamar
- * aranc  {this}  contexti  kirarutyan
- * **/
+import CustomBox from "./components/custom-input";
 
 function App() {
-  /**
-   * Functional component vortex  cankacac tesaki  informacia  ogitagorcelu,
-   * popoxelu hamar vorpeszi react@ haskana  ayd  tvyalnberi popoxutyunnern
-   * kirarum eqn useState() hook@  vor@ irenic nerkayacnum e  mi  global  funkcia
-   * ayn tuyl e talis stexcel  2 paramter, voroncic arajin@  aynd  popoxakann e  vor@
-   * karox e linel inchpes primitiv aynpes el  object  tesaki popoxakan
-   * isk ekrord parametr@ da  ayn  funkcian e  vori mijocov  popoxuma  haytararvac  popoxakani arjeq@  kam arjeqner@
-   * const [showBox, setShowBox] = useState(true)  kirarutyunn
-   * usestate@ () nshani  nersum spasum e  naxnakan  arjeq@  kaxvac popoixakani tesakic,  string, number, booleanr,  kam object
-   *
-   * **/
+  // nmab dzevi  haytararutyunner  shaat hazvadep en anum kariqic elnelov
+  const x = 34
 
-  const [showBox, setShowBox] = useState(true)
+  const [productList, setProductList] = useState([])
+  const [pageLoader, setPageLoader] = useState(false)
 
-  const handleHideBox = ()=>{
-    setShowBox(!showBox)
-  }
+  useEffect(() => {
+    //  ComponentDidMount
+    // Ashxatum e  @ndamenn  mek angam  3  varkyan  heoto mez talov mor zangvac
+    setPageLoader(true)
+    setTimeout(() => {
+      setProductList([
+        {
+          name: 'Test 1',
+          date: '20/11/2022'
+        },
+        {
+          name: 'Test 2',
+          date: '20/11/2022'
+        },
+        {
+          name: 'Test 3',
+          date: '20/11/2022'
+        }
+      ])
+    }, 3000)
+  }, [])
+
+
+  useEffect(() => {
+    //  ComponentDidUpdate
+    // Ashxatum e  erb productneri listi het  popoxutyun e texi unece staytum
+    console.log(productList)
+    if (productList.length) {
+      setPageLoader(false)
+    }
+  }, [productList])
+
+
   return <div>
-    {/*{showBox?  <CardBox title={'title 1'} description={'description 1'}/>: null}*/}
-
-    <TabList/>
-
-    {/*<button onClick={handleHideBox}>hide Box</button>*/}
-
+    {!pageLoader ? <div>
+      {productList.length ? productList.map((item, index) => {
+        return <CustomBox key={index} data={item}/>
+      }) : <p>Empty Product List</p>}
+    </div> : <p>Loading...</p>}
   </div>
 }
 
-
 export default App
-
-/**
- * Class componenti  orinak
- *
- * class  App extends React.Component{
- *   render() {
- *     return <div>
- *       example  information
- *     </div>
- *   }
- * }
- *
- * **/
-
-/**
- * Functional component tarberak 1
- *
- * function App(){
- *   return <div>
- *     example information
- *   </div>
- * }
- *
- * **/
-
-/**
- * Functional component tarberak 2
- *
- * const  App = ()=>{
- *   return <div>
- *     example information
- *   </div>
- * }
- *
- * **/
